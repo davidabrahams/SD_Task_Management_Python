aa5eaa4f35656460c0b35a95b737a6ccc6fb457d
David <david.abrahams@students.olin.edu>
2015-03-04 11:49:43 -0500

    Made small updates

--- name_processes.py
+++ name_processes.py
@@ -1,1 +1,23 @@
+import psutil
+import time
+
 __author__ = 'davidabrahams'
+
+
+def print_processes():
+    for proc in psutil.process_iter():
+        proc.get_cpu_percent()
+
+    time.sleep(1)
+
+    for proc in psutil.process_iter():
+        memory_info, vms = proc.get_memory_info()
+        print str(proc.get_cpu_percent()) + ', ' + str(memory_info) + ', ' + str(proc.name())
+
+
+def get_procs():
+    return [proc for proc in psutil.process_iter()]
+
+
+if __name__ == '__main__':
+    print_processes()
