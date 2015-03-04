478be7087d5f3c4d7776b3b7983cd7650070ecf0
Tom <tah20112@gmail.com>
2015-03-04 12:50:32 -0500

    Prettier

--- name_processes.py
+++ name_processes.py
@@ -16,19 +16,15 @@ def get_process_data():
 
     for proc in psutil.process_iter():
         memory_info, vms = proc.get_memory_info()
-        item = [int(proc.get_cpu_percent()), (int(memory_info))/(1024.0**2)]
         process_names.append(str(proc.name()))
-        process_usages.append(item)
+        process_usages.append([int(proc.get_cpu_percent()), (int(memory_info))/(1024.0**2)])
     process_data = dict(zip(process_names,process_usages))
     return process_data
 
 def print_processes():
     process_data = get_process_data()
     for key in process_data:
-        print key, process_data[key]
-
-# So I got rid of print_processes(). It's in older versions if we need it. I didn't do it deliberately...
-
+        print process_data[key], key
 
 if __name__ == '__main__':
     print_processes()
