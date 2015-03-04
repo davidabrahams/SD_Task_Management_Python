e296ea4b0a82ee03fd453f79486ac62ee0c0c337
David <david.abrahams@students.olin.edu>
2015-03-04 16:08:17 -0500

    fixed initialization

--- process_manager.py
+++ process_manager.py
@@ -3,19 +3,13 @@ import time
 
 
 class ProcessManager:
+
     def get_process_data(self):
         """
         :return: a dict of process_name:(cpu_percent, ram_usage_mb) key value pairs
         """
-
-        # Check the initial usage of all processes
-        for proc in psutil.process_iter():
-            proc.get_cpu_percent()
         process_names = []
         process_usages = []
-
-        # Sleep an arbitrary amount of time in between processes checks
-        time.sleep(0.5)
 
         for proc in psutil.process_iter():
             # Iterate through the processes and add the name:(cpu, ram) pairs to lists
@@ -27,20 +21,34 @@ class ProcessManager:
         return dict(zip(process_names, process_usages))
 
 
-def update(self):
-    data = self.data
-    new_dict = self.get_process_data()
-    for key in data:
-        if key in new_dict:
-            data[key] = new_dict[key]
-        else:
-            del data[key]
-    for key in new_dict:
-        if key not in data:
-            data[key] = new_dict[key]
+
+    def init_process_data(self):
+        """
+        :return: a dict of process_name:(cpu_percent, ram_usage_mb) key value pairs
+        """
+
+        # Check the initial usage of all processes
+        for proc in psutil.process_iter():
+            proc.get_cpu_percent()
+
+            time.sleep(0.5)
+
+        return self.get_process_data(self)
+
+    def update(self):
+        data = self.data
+        new_dict = self.get_process_data()
+        for key in data:
+            if key in new_dict:
+                data[key] = new_dict[key]
+            else:
+                data.pop(key)
+        for key in new_dict:
+            if key not in data:
+                data[key] = new_dict[key]
 
     def __init__(self):
-        self.data = self.get_process_data()
+        self.data = self.init_process_data(self)
 
 
 if __name__ == '__main__':
