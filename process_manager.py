e568311bb40021ace5bdea4915e039edc513e521
David <david.abrahams@students.olin.edu>
2015-03-04 15:51:43 -0500

    reformatted code

--- process_manager.py
+++ process_manager.py
@@ -1,46 +1,47 @@
 import psutil
 import time
 
+
 class ProcessManager:
+    def get_process_data(self):
+        """
+        :return: a dict of process_name:(cpu_percent, ram_usage_mb) key value pairs
+        """
 
-	def get_process_data(self):
-	    """
-	    :return: a dict of process_name:(cpu_percent, ram_usage_mb) key value pairs
-	    """
+        # Check the initial usage of all processes
+        for proc in psutil.process_iter():
+            proc.get_cpu_percent()
+        process_names = []
+        process_usages = []
 
-	    # Check the initial usage of all processes
-	    for proc in psutil.process_iter():
-	        proc.get_cpu_percent()
-	    process_names = []
-	    process_usages = []
+        # Sleep an arbitrary amount of time in between processes checks
+        time.sleep(0.5)
 
-	    # Sleep an arbitrary amount of time in between processes checks
-	    time.sleep(0.5)
+        for proc in psutil.process_iter():
+            # Iterate through the processes and add the name:(cpu, ram) pairs to lists
+            memory_info, vms = proc.get_memory_info()
+            process_names.append(str(proc.name()))
+            process_usages.append((proc.get_cpu_percent(), (int(memory_info)) / (1024.0 ** 2)))
 
-	    for proc in psutil.process_iter():
-	        # Iterate through the processes and add the name:(cpu, ram) pairs to lists
-	        memory_info, vms = proc.get_memory_info()
-	        process_names.append(str(proc.name()))
-	        process_usages.append((proc.get_cpu_percent(), (int(memory_info)) / (1024.0 ** 2)))
+            # create a dict out of the lists
+        return dict(zip(process_names, process_usages))
 
-	    #create a dict out of the lists
-	    return dict(zip(process_names, process_usages))
 
-    def update(self):
-    	data = self.data
-    	new_dict = self.get_process_data()
-    	for key in data:
-    		if key in new_dict:
-    			data[key] = new_dict[key]
-    		else:
-    			del data[key]
-    	for key in new_dict:
-    		if key not in data:
-    			data[key] = new_dict[key]
+def update(self):
+    data = self.data
+    new_dict = self.get_process_data()
+    for key in data:
+        if key in new_dict:
+            data[key] = new_dict[key]
+        else:
+            del data[key]
+    for key in new_dict:
+        if key not in data:
+            data[key] = new_dict[key]
 
-	def __init__(self):
-		self.data = self.get_process_data()
+    def __init__(self):
+        self.data = self.get_process_data()
 
 
 if __name__ == '__main__':
-	proc_manager = ProcessManager()
+    proc_manager = ProcessManager()
