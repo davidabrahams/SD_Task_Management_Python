5ae4251652634eb2f24aa6ff7356c9719ce5eaf6
David <david.abrahams@students.olin.edu>
2015-03-09 22:23:10 -0400

    added button functionality

--- PyChartApp.py
+++ PyChartApp.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Button
 import seaborn as sns
 import time
 from ProcessManager import ProcessManager
@@ -11,6 +12,8 @@ class PyChartApp:
         self.proc_manager = ProcessManager()
         self.fig = plt.figure('Active Process RAM Usage')
         self.ax = self.fig.add_subplot(111)
+        self.selected_pid = None
+        self.viewing_ram = True
 
 
     def shorten_names(self, names):
@@ -35,38 +38,67 @@ class PyChartApp:
         vals = []
         for i, k in enumerate(keys):
             pid, cpu, ram = data[k]
-            if ram < 20.0:
+            if (self.viewing_ram and ram < 20.0) or (not self.viewing_ram and cpu < 1.0):
                 indices_to_remove.append(i)
             else:
-                vals.append(ram)
+                if self.viewing_ram:
+                    vals.append(ram)
+                else:
+                    vals.append(cpu)
         # Iterate backward in order to remove correct indices.
         for i in reversed(indices_to_remove):
             keys.pop(i)
 
         names = self.shorten_names([data[k][0] for k in keys])
 
+        explode_list = [0] * len(keys)
+        if self.selected_pid in keys:
+            explode_list[keys.index(self.selected_pid)] = 0.2
+
         # Make a pie graph
         plt.clf()
-        wedges, pie_labels = plt.pie(vals, labels=names)
+        wedges, pie_labels = plt.pie(vals, labels=names, explode=explode_list)
         self.wedge_dict = dict(zip(wedges, keys))
-        self.ax.axis('equal')
         self.make_picker(self.fig, wedges)
 
+        # Make a button
+        term_button_ax = plt.axes([0.52, 0.01, 0.2, 0.07])
+        button = Button(term_button_ax, 'Terminate')
+        button.on_clicked(self.terminate)
+
+        #Make another button
+        switch_button_ax = plt.axes([0.28, 0.01, 0.2, 0.07])
+        if not self.viewing_ram:
+            button_switch = Button(switch_button_ax, 'Switch to RAM')
+        else:
+            button_switch = Button(switch_button_ax, 'Switch to CPU')
+        button_switch.on_clicked(self.switch)
+
+        self.ax.axis('equal')
+
         toc = time.clock()
-        sleep_time = 0.25
+        sleep_time = 1
         plt.pause(max([sleep_time - (toc - tic), 0.0001]))
 
     def make_picker(self, fig, wedges):
 
         def onclick(event):
             wedge = event.artist
-            self.proc_manager.terminate_process(self.wedge_dict[wedge])
+            pid = self.wedge_dict[wedge]
+            self.selected_pid = pid
 
-    # Make wedges selectable
+        # Make wedges selectable
         for wedge in wedges:
             wedge.set_picker(True)
 
         fig.canvas.mpl_connect('pick_event', onclick)
+
+    def terminate(self, event):
+        self.proc_manager.terminate_process(self.selected_pid)
+
+    def switch(self, event):
+        self.viewing_ram = not self.viewing_ram
+
 
 if __name__ == '__main__':
     p = PyChartApp()
